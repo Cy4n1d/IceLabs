ca2d3fbd8fc5e5072acd5f6c4df337751f791958
Sven Lütje <sven.luetje@googlemail.com>
2014-09-12 00:46:13 +0200

    add helper library

---
+++ application/libraries/helper.php
@@ -0,0 +1,19 @@
+<?php
+
+class Config {
+	
+	/**
+	 * Return the given date as sql conform string
+	 * 
+	 * @param DateTime $date
+	 * @return string
+	 */
+	public function get_sql_formatted_date(DateTime $date) {
+		if(is_null($date)) {
+			return '0000-00-00 00:00:00';
+		}
+		
+		return $date->format("Y-m-d H:i:s");
+	}
+	
+}
